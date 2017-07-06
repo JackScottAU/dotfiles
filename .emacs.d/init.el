14ffb4395e7870c9d403ad442e18e17a26817d66
Jack Scott <jack@jackscott.id.au>
2017-07-06 11:59:43 +1000

    Change emacs startup mode.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,1 +1,8 @@
+;; Don't show the menu bar. It's just a waste of screen space.
 (menu-bar-mode -1)
+
+(custom-set-variables
+ ;; Start with a scratch buffer upon opening emacs.
+ '(initial-buffer-choice t))
+
+(custom-set-faces)
