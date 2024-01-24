3c48d7cad48db417d09f80a46bec949e2c995ae4
Jack Scott <jack@jackscott.id.au>
2024-01-24 22:37:22 +0000

    Added basic install script.

---
+++ dotfiles/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd $HOME
+ln -s ./dotfiles/.config .config
+ln -s ./dotfiles/.emacs.d .emacs.d
+ln -s ./dotfiles/.bash_login .bash_login
+ln -s ./dotfiles/.bash_logout .bash_logout
+ln -s ./dotfiles/.bashrc .bashrc
+
